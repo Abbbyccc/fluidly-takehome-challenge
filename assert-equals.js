e67fac61d0271dc566c8e1909b0798e52362b9ed
Abbbyccc <abbychien@hotmail.com>
2022-06-20 15:08:38 +0100

    change to throw error

--- assert-equals.js
+++ assert-equals.js
@@ -1,13 +1,10 @@
 function assertEquals(expect, actual) {
     if (typeof expect !== typeof actual && !Array.isArray(expect) && !Array.isArray(actual)) {
-        console.error(`Expected type ${typeof expect} but found type ${typeof actual}`)
-        return
+        throw new Error(`Expected type ${typeof expect} but found type ${typeof actual}`)
     } else if (Array.isArray(expect) && !Array.isArray(actual)) {
-        console.error(`Expected type array but found type ${typeof actual}`)
-        return
+        throw new Error(`Expected type array but found type ${typeof actual}`)
     } else if (!Array.isArray(expect) && Array.isArray(actual)) {
-        console.error(`Expected type ${typeof expect} but found type array`)
-        return
+        throw new Error(`Expected type ${typeof expect} but found type array`)
     }
 
 
@@ -15,31 +12,32 @@ function assertEquals(expect, actual) {
         if (expect === actual) {
             console.info('No error')
         } else {
-            console.error(`Expected ${expect} but found ${actual}`)
-            return
+            throw new Error(`Expected ${expect} but found ${actual}`)
         }
     }
 
+    
     if (typeof expect === 'string' && typeof actual === 'string') {
         if (expect === actual) {
             console.info('No error')
         } else {
-            console.error(`Expected "${expect}" but found "${actual}"`)
-            return
+            throw new Error(`Expected "${expect}" but found "${actual}"`)
+
         }
     }
+
 
     if (Array.isArray(expect) && Array.isArray(actual)) {
         if (expect.length === actual.length) {
             for (let i = 0; i < expect.length; i++) {
                 if (expect[i] !== actual[i]) {
-                    console.error(`Expected "${expect[i]}" but found "${actual[i]}"`)
+                    throw new Error(`Expected "${expect[i]}" but found "${actual[i]}"`)
                 }
             }
             console.info('No error')
         }
     } else if (expect.length !== actual.length) {
-        console.error(`Expected array length ${expect.length} but found ${actual.length}`)
+        throw new Error(`Expected array length ${expect.length} but found ${actual.length}`)
     }
 }
 
